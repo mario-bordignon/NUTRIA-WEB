2c8f9c803c92941a98ed45666ef2d1e7a42a62c0
mario-bordignon <m.bordignon@aluno.ifsp.edu.br>
2026-05-17 01:20:50 -0300

    tela-1 (1.1)

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-/**
- * Lógica de interatividade do front-end.
- */
-
 document.addEventListener('DOMContentLoaded', () => {
-    // Inicialização da estrutura base.
-    // Lógicas futuras de interatividade (como modais ou requisições) devem ser inseridas aqui.
+    
 });
